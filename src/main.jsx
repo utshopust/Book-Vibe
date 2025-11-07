a038e2363b18cde7b9c96fe5051b8bb3ccfe1371
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-11-07 20:43:17 +0600

    Create Navbar for Book Vibe

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { RouterProvider } from "react-router/dom";
+import { router } from './Routes/routes.jsx';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex items-center justify-center h-screen  '>
+            <h1 className='text-7xl text-center'>404: Page Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
